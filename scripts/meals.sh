5dfca276399d5413d8bc31616828d4b335e5a2eb
Joshua Taylor <Joshuataylora@gmail.com>
2022-12-29 10:21:48 -0500

    Removed script based seeding and added properties to application.properties file to utilize data.sql and schema.sql to generate tables and seed in memory h2 database

--- scripts/meals.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -d '{"timestamp":"2022-07-05T06:12:40Z"}' -H "Content-Type: application/json" http://localhost:8080/api/v1/meals
-curl -d '{"timestamp":"2022-07-05T09:12:40Z"}' -H "Content-Type: application/json" http://localhost:8080/api/v1/meals
-curl -d '{"timestamp":"2022-07-05T12:12:40Z"}' -H "Content-Type: application/json" http://localhost:8080/api/v1/meals
-curl -d '{"timestamp":"2022-07-05T15:12:40Z"}' -H "Content-Type: application/json" http://localhost:8080/api/v1/meals
-curl -d '{"timestamp":"2022-07-05T18:12:40Z"}' -H "Content-Type: application/json" http://localhost:8080/api/v1/meals
-
--- scripts/meds.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl http://localhost:8080/api/v1/medications -H "Content-Type: application/json" -d '{"day_id": 1,"name": "Prednisone","quantity": 1, "strength": 20,"dosage_unit":"mg"}'
